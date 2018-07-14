70b33a3ae668f7afaf6bd019977434b2242fce57
Nathan Chancellor <natechancellor@gmail.com>
2018-07-14 11:01:39 -0700

    param_read_write: Add a cmdline parameter for cust_flag
    This value needs to be set as soon as the device boots otherwise
    certain aspects of the UI will be set before the user can toggle
    it back.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/param_read_write/param_read_write.c
+++ drivers/param_read_write/param_read_write.c
@@ -4,7 +4,16 @@
  * Made by pappschlumpf (Erik Müller)
  */
 
+#include <linux/init.h>
 #include <linux/moduleparam.h>
 
-static unsigned int cust_flag = 3;
+static unsigned int cust_flag = 0;
+
+static int __init set_cust_flag(char *cmdline)
+{
+	cust_flag = 3;
+	return 0;
+}
+__setup("avengers_theme", set_cust_flag);
+
 module_param(cust_flag, uint, 0644);

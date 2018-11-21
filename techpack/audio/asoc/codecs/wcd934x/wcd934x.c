6decbcfaaa66b0ef4de1cf526a32e230f1237653
flar2 <asegaert@gmail.com>
2018-11-20 23:51:29 -0500

    wcd934x: remove speaker gain settings for 6T

--- techpack/audio/asoc/codecs/wcd934x/wcd934x.c
+++ techpack/audio/asoc/codecs/wcd934x/wcd934x.c
@@ -10253,7 +10253,6 @@ static struct kobj_attribute speaker_gain_attribute =
 static struct attribute *sound_control_attrs_6T[] = {
 		&mic_gain_attribute.attr,
 		&earpiece_gain_attribute.attr,
-		&speaker_gain_attribute.attr,
 		NULL,
 };
 
